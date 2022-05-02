04466265cafa2faa9eaa26f563f1ba42b8193840
Maksym-Lizohub <maksym.lizohub@gmail.com>
2022-05-03 01:22:15 +0300

    test

--- css/js/modal.js
+++ css/js/modal.js
@@ -1,4 +1,5 @@
 (() => {
+    const newLocal = "[data-modal-close]";
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
